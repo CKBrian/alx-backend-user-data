af1c6b67d6c6c170112d1b05ad3b23c6b83d1a57
Brian <chumobrian@gmail.com>
2024-05-16 20:57:47 +0300

    Feat(get_db): Add a  function that returns a connector to the database (mysql.connector.connection.MySQLConnection object).

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -65,7 +65,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> MySQLConnection:
     '''returns a connector to the database
        (mysql.connector.connection.MySQLConnection object'''
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')

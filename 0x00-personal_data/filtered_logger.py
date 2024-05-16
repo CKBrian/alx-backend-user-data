79ea846d5f29bcb81abc660809adf264bb99ef81
Brian <chumobrian@gmail.com>
2024-05-16 15:39:53 +0300

    Feat(filter_datum): Add a script with a function that eturns the log message obfuscated

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Defines a filtered_logger module with functions'''
+import re
+
+
+def filter_datum(fields: list, redaction: str,
+                 message: str, separator: str) -> str:
+    '''
+        returns the log message obfuscated
+
+        Args:
+            fields (list): list of strings repr all fields to obfuscate
+            redaction (str): a string representing by what the field will be
+                             obfuscated
+            message (str): a string representing the log line
+            separator (str): a string representing by which character is
+                             separating all fields in the log line (message)
+    '''
+    # filter out parts to be obfuscaated
+    for field in fields:
+        message = re.sub(fr'{field}=[\w/]+', f'{field}={redaction}', message)
+    return message

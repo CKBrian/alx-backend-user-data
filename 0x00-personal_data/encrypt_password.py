6a145e93e83ea04a9fd2a24304c52c7bee2665e6
Brian <chumobrian@gmail.com>
2024-05-16 23:33:58 +0300

    Feat(hash_password): Add a  function that returns bytes of encryoted password.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+'''Defines a module with functions that perform encryption'''
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    '''function that expects one string argument name password
+       and returns a salted hashed password, which is a byte string.'''
+    # bcrypt salt
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed

d16f9798e1bac5248dd38e9e57c22bc0cf24de0a
Brian <chumobrian@gmail.com>
2024-05-21 15:39:48 +0300

    feat: Add an Auth class module template for all authentication system

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+api/v1/auth/auth.py
+'''Auth module
+'''
+import request from flask
+
+
+class Auth:
+    '''Auth class'''
+    def require_auth(self, path: str, excluded_paths:List[str]) -> bool:
+       '''returns False - path
+       '''
+       return False
+
+    def authorization_header(self, request=None) -> str:
+       '''that returns None - request'''
+       return request
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''that returns None - request'''
+        return request

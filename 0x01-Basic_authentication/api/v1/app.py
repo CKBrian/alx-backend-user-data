732cec227f33f5c0cda633c3cd8b6452c7ed199a
Brian <chumobrian@gmail.com>
2024-05-21 18:34:44 +0300

    feat(authorization_header): Add an Auth class method that validates all requests to secure the API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,7 +12,10 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+auth = getenv("AUTH_TYPE", None)
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -33,6 +36,24 @@ def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    ''' Before request handler to check authorization '''
+    excluded_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if auth:
+        is_excluded = auth.require_auth(request.path, excluded_list)
+        if not is_excluded:
+            auth_header = auth.authorization_header(request)
+            if not auth_header:
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,7 @@ class Auth:
     '''Auth class'''
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        '''returns False - path
+        '''returns False if a path is in excluded_paths
         '''
         if not path or not excluded_paths:
             return True
@@ -20,7 +20,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         '''that returns None - request'''
-        return request
+        headers = request.headers
+        return (headers.get('Authorization', None)
+                if headers else None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''that returns None - request'''

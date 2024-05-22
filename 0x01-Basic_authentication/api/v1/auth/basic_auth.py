67eebe67ff048fc99f05901b70d9d631b5d3dc87
Brian <chumobrian@gmail.com>
2024-05-22 04:13:46 +0300

    feat(BasicAuth): Add a BasicAuth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''BasicAuth module'''
+
+from api.v1.auth.auth import Auth
+class BasicAuth(Auth):
+    '''BasicAuth class'''
+
+    def __init__(self):
+       '''Initializes Auth class'''
+        super().__init__(self)

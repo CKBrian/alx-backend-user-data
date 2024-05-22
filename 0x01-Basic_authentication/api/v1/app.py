2d887228ba8c28a77aee8cfad8427fe7a05de6d8
Brian <chumobrian@gmail.com>
2024-05-22 09:37:02 +0300

    Refactor: reefactor if else statement

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -51,7 +51,7 @@ def before_request():
         '/api/v1/forbidden/'
     ]
     if auth:
-        if not auth.require_auth(request.path, excluded_list):
+        if auth.require_auth(request.path, excluded_list):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:

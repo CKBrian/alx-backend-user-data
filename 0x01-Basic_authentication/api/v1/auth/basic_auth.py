f639ceda95a6755a674c4eff3b542e333b0f138c
Brian <chumobrian@gmail.com>
2024-05-22 12:21:54 +0300

    feat(current_user): Add a BasicAuth class method that overloads Auth and retrieves the User instance for a request

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,11 +46,12 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
         '''returns the User instance based on his email and password.'''
-        if not user_email and not isinstance(user_email, str):
+        if not user_email or not isinstance(user_email, str):
             return None
-        if not user_pwd and not isinstance(user_pwd, str):
+        if not user_pwd or not isinstance(user_pwd, str):
             return None
-        users = User.search({'email': user_email})
+        user = User()
+        users = user.search({'email': user_email})
         if users:
             if users[0].is_valid_password(user_pwd):
                 return users[0]

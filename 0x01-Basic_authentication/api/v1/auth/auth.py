28504e38dec5dad8930a51f9fef60dc4516b97e2
Brian <chumobrian@gmail.com>
2024-05-22 09:35:54 +0300

    feat(require_auth): Add an Auth class method that verifies if a path is in excluded_paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,9 +20,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         '''that returns None - request'''
-        # headers = request.headers
-        return (request.headers.get('Authorization', None)
-                if request else None)
+        if request:
+            return request.headers.get('Authorization', None)
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''that returns None - request'''

2e8f62ed01742e46dae012238e47ed589dc553b6
Brian <chumobrian@gmail.com>
2024-05-22 05:01:01 +0300

    feat(BasicAuth): Add a BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 '''BasicAuth module'''
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -11,8 +12,21 @@ class BasicAuth(Auth):
             self,
             authorization_header: str) -> str:
         '''returns the Base64 part of the Authorization
-        header for a Basic Authentication'''
+           header for a Basic Authentication'''
         header = authorization_header
         if header and isinstance(header, str) and header.startswith('Basic '):
             return authorization_header[len('Basic '):]
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        '''returns the decoded value of a Base64 string
+           base64_authorization_header:'''
+        auth_header = base64_authorization_header
+        try:
+            if not auth_header and not isinstance(auth_header, str):
+                None
+            decoded = base64.b64decode(auth_header, validate=True)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None

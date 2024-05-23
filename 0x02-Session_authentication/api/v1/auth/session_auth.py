f35c753956bb59e7f30d0aa6339ab80cf730e511
Brian <chumobrian@gmail.com>
2024-05-23 22:18:30 +0300

    Feat(create_session): Add a method that creates a session id in the SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,20 @@
 '''SessionAuth module'''
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     '''SessionAuth Class'''
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''Return:
+                users session Id
+        '''
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.__class__.user_id_by_session_id[session_id] = user_id
+        return session_id

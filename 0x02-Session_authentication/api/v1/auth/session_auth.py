d5801b9f47335d0c151fc150fced836328e8a8d5
Brian <chumobrian@gmail.com>
2024-05-24 00:02:33 +0300

    Feat(current_user): Add a method that Creates a user based on session id in the SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -21,7 +22,16 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        '''returns a User ID based on a Session ID'''
+        '''Returns:
+               a User ID based on a Session ID
+        '''
         if not session_id or not isinstance(session_id, str):
             return None
         return self.__class__.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        '''Returns:
+               the current user object'''
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)

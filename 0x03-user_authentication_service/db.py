f6e8674dddf9172a948a520c98229466409d2424
Brian <chumobrian@gmail.com>
2024-05-29 22:09:38 +0300

    feat(db): Add DB class and its methods
    - Added DB class to handle database operations
    - Implemented methods to add and find users
    - Added property to memoize session object

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,64 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from typing import Dict, Any
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Returns a User object
+        """
+        session = self._session
+        user = User(email=email, hashed_password=hashed_password)
+        session.add(user)
+        session.commit()
+        return user
+
+    def find_user_by(self, **kwargs: Dict[str, Any]) -> User:
+        """
+        Finds a user by the given keyword arguments.
+
+        Args:
+            **kwargs: Keyword arguments representing the columns and their values to filter by.
+
+        Returns:
+            User: The user object matching the given keyword arguments.
+
+        Raises:
+            NoResultFound: If no user is found.
+            InvalidRequestError: If there is an invalid request.
+        """
+        try:
+            session = self._session
+            user = session.query(User).filter_by(**kwargs).first()
+            if not user:
+                raise NoResultFound()
+        except (InvalidRequestError, NoResultFound) as e:
+            raise e
+        return user

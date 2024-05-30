a33a9208cbc25f62cab93fc947b37479abaa4983
Brian <chumobrian@gmail.com>
2024-05-30 22:40:54 +0300

    feat(valid_login): Add an Auth class method that validates a user on login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -22,6 +22,14 @@ class Auth:
             user = self._db.add_user(email, _hash_password(password))
             return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        '''Validates a users password'''
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     '''Returns a hashed passwd'''

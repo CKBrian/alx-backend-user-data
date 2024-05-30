47a0b8e1867be5365e06550d7759f9943e4c1dc7
Brian <chumobrian@gmail.com>
2024-05-30 23:59:49 +0300

    Feat(create_session): Defines a flask app method that validates and assigns a user a session id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''Defines an app module'''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask('__name__')
@@ -27,5 +27,17 @@ def create_user():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def get_session():
+    '''Validates a user and assigns a session_id'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    if AUTH.create_session(email):
+        return jsonify({"email": "<user email>", "message": "logged in"})
+    abort(401)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)

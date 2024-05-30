d0639ce560ea4285816360254fe45cd6e45791b5
Brian <chumobrian@gmail.com>
2024-05-30 18:23:31 +0300

    feat(Auth): Add an Auth class that handles user authentication

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+'''Defines an Auth module'''
+
+import bcrypt
+from db import DB, NoResultFound, User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''Registers a User'''
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User <user's email> already exists")
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+            return user
+
+
+def _hash_password(password: str) -> bytes:
+    '''Returns a hashed passwd'''
+    if password:
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode('utf-8'), salt)

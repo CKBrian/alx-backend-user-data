b2d571889dc684b19d4df05737fd17df20dd2c46
Brian <chumobrian@gmail.com>
2024-05-31 02:44:05 +0300

    Feat(get_reset_password_token): Defines a flask route method that generates an account reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,7 +67,7 @@ def profile():
     abort(403)
 
 
-@app.route('/reset_password', methods=['GET'])
+@app.route('/reset_password', methods=['POST'])
 def get_reset_password_token():
     try:
         email = request.form.get('email')

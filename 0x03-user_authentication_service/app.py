a8cad3cec7aeeff082097b5e398fe93f55b5b9e2
Brian <chumobrian@gmail.com>
2024-05-30 19:07:03 +0300

    Feat(create_user): Defines a flask app method that registers a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 
 '''Defines an app module'''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask('__name__')
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def get_user():
     '''Returns a json payload'''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    '''Registers a user'''
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        AUTH.register_user(email, password)
+        return jsonify({"email": "<registered email>",
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
